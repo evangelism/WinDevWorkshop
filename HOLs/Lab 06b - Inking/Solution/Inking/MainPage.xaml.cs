7c6cfa2e1d2b84215dc81b329262cdb930aa877f
AndyWigley <andy.wigley@microsoft.com>
2015-09-29 14:23:27 +0100

    Draft of Inking solution

---
+++ HOLs/Lab 06b - Inking/Solution/Inking/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input.Inking;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Inking
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        int penSize = 4;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            InkDrawingAttributes drawingAttributes = new InkDrawingAttributes();
+            drawingAttributes.Color = Windows.UI.Colors.Red;
+            drawingAttributes.Size = new Size(penSize, penSize);
+            drawingAttributes.IgnorePressure = false;
+            drawingAttributes.FitToCurve = true;
+
+            InkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+            InkCanvas.InkPresenter.InputDeviceTypes = Windows.UI.Core.CoreInputDeviceTypes.Mouse | Windows.UI.Core.CoreInputDeviceTypes.Pen | Windows.UI.Core.CoreInputDeviceTypes.Touch;
+        }
+
+        void OnClear(object sender, RoutedEventArgs e)
+        {
+            InkCanvas.InkPresenter.StrokeContainer.Clear();
+            //rootPage.NotifyUser("Cleared Canvas", NotifyType.StatusMessage);
+        }
+
+        void OnPenColorChanged(object sender, RoutedEventArgs e)
+        {
+            if (InkCanvas != null)
+            {
+                InkDrawingAttributes drawingAttributes = InkCanvas.InkPresenter.CopyDefaultDrawingAttributes();
+
+                // Use button's background to set new pen's color
+                var btnSender = sender as Button;
+                var brush = btnSender.Background as Windows.UI.Xaml.Media.SolidColorBrush;
+
+                drawingAttributes.Color = brush.Color;
+                InkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+            }
+        }
+
+        private void ErasingModeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            InkCanvas.InkPresenter.InputProcessingConfiguration.Mode = InkInputProcessingMode.Erasing;
+        }
+
+        private void ErasingModeCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            InkCanvas.InkPresenter.InputProcessingConfiguration.Mode = InkInputProcessingMode.Inking;
+        }
+    }
+}
